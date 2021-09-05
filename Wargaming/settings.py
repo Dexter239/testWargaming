4c79ca30f70ce473349d5f83643763c7e081d953
Dexter239 <klim_iliya@mail.ru>
2021-09-05 20:01:26 +0300

    release ver

--- Wargaming/settings.py
+++ Wargaming/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)b&6s(045@!%-fm!-#7px5_h3)c3#7yxe05@a1*47rmbo5g)ya'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
